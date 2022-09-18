779c8c053c3749dc2389c5d0d7d272594e5a345e
jeongwon <jeongwonlee0@gmail.com>
2022-09-18 19:37:36 +0900

    Initial commit

---
+++ S3fileupload/src/test/java/javaproject/S3fileupload/S3fileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package javaproject.S3fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3fileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
